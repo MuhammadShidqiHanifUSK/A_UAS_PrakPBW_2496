25a3ae22998bd9ee0e37ab46e114763d67be2ed5
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2026-05-22 16:10:20 +0700

    feat: tambah Model Setoran dengan relasi

---
+++ app/Models/Setoran.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setoran extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'tanggal',
+        'paraf_guru',
+        'paraf_ortu',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sabaq()
+    {
+        return $this->hasMany(Sabaq::class);
+    }
+
+    public function sabqi()
+    {
+        return $this->hasMany(Sabqi::class);
+    }
+
+    public function manzil()
+    {
+        return $this->hasMany(Manzil::class);
+    }
+
+    public function catatanSetoran()
+    {
+        return $this->hasMany(CatatanSetoran::class);
+    }
+}
